b81cc3ff056cd263db75c5b6b99b5799f16ec15f
Jzone96 <jason.liberti@live.it>
2021-05-31 21:27:46 +0200

    implemented firebase utils, included ability to store users in firestore database

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends Component {
   constructor(){
@@ -66,11 +66,23 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{//persistant user session
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{//persistant user session
       /*this function will create a message channel between our app and firebase. when a user signout, 
       this information will be send here, otherwise, it will assume that the user is still signed in*/
-      this.setState({currentUser:user})
-      console.log(user)
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }else{
+        this.setState({currentUser: userAuth});
+      }
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,3 +1,44 @@
+/*request for firestore database data
+in a new.file.js
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+const firestore = firestore.firestore();
+
+//firebase database consist in a nassive json file, with collections and documents:
+//collection: is a collection of items (like users, or cart item)
+//documents: is an element of a collection
+
+//chain way
+firestore.collection('users').doc('qchLJVFqLzFykX3HA4Hi').collection('cartItems').doc('cayk1fSkKTnhFsYnQ19J');
+//point directly to document
+firestore.doc('/users/qchLJVFqLzFykX3HA4Hi/cartItems/cayk1fSkKTnhFsYnQ19J');
+//point directly to collection
+firestore.collection('/users/qchLJVFqLzFykX3HA4Hi/cartItems')
+-----------------------------------------------------------------------------------
+query reference and query snapshot (returns a objects)
+
+REFERNCE = object that rapresent the current place we are querying
+ex
+firestore.doc('/users/qchLJVFqLzFykX3HA4Hi/cartItems/cayk1fSkKTnhFsYnQ19J');
+
+doesn't have actual data of the collection or document. it has proprieties that tells us detail 
+(id and path of the query) about it
+or the method to get the snapshot object that will give us the data we are looking for.
+
+documentRef object are used to perform set(), get(), update(), delete() method
+
+to obtain snapshotObj (that contains the actual data) we use get();
+
+this is a document reference and return document snapshot object
+firestore.doc('/users/qchLJVFqLzFykX3HA4Hi/cartItems/cayk1fSkKTnhFsYnQ19J');
+
+this is a collection reference and return query snapshot object
+firestore.collection('/users/qchLJVFqLzFykX3HA4Hi/cartItems')
+
+*/
+
 import firebase from 'firebase/app';//pulling in the firebase utility library
 //firebase has a lot of dependencies, and we only take the ones we need
 import 'firebase/firestore';// db storage
@@ -11,8 +52,36 @@ const config={
     messagingSenderId: "140980742378",
     appId: "1:140980742378:web:2e8c33d2f5645704100f93",
     measurementId: "G-X5Z25N7GLF"
-  };
+};
 
+//take the userAuth object from google signin and store it inside database
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    //save to database only if we get back a valid object
+    if (!userAuth) return;
+
+    //check if user already exist
+    const userRef = firestore.doc(`users/${userAuth.uid}`);//query reference
+
+    const snapShot = await userRef.get();//snapshot, rapresents the data. 
+        //to check if there is some data there, we use the exists propriety
+    if (!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch (err){
+            console.log('error creating user', err)
+        }
+    }
+
+    return userRef
+}
  firebase.initializeApp(config);
 
  export const auth = firebase.auth();
@@ -23,3 +92,4 @@ const config={
  export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
  export default firebase;
+
